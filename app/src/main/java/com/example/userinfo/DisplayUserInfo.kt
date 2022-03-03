fa485f85866084ecff54a6738aa091ca6503c6cf
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-03-03 15:59:11 +0530

    add string template

--- app/src/main/java/com/example/userinfo/DisplayUserInfo.kt
+++ app/src/main/java/com/example/userinfo/DisplayUserInfo.kt
@@ -1,10 +1,13 @@
 package com.example.userinfo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.res.Resources
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class DisplayUserInfo : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_user_info)
@@ -13,8 +16,7 @@ class DisplayUserInfo : AppCompatActivity() {
         val address = intent.getStringExtra(Address)
         val phoneNumber = intent.getStringExtra(PhoneNumber)
         val pinCode=intent.getStringExtra(PinCode)
-        val message="Hi "+userName+", How are you? Are you staying at "+ address +"-"+pinCode+"code. I am not able to contact you on"+phoneNumber+". Can I " +
-                "email you the details at "+email
+        val message=getString(R.string.displayUserInfo,userName,address,pinCode,phoneNumber,email)
         findViewById<TextView>(R.id.display).text=message
     }
 }
