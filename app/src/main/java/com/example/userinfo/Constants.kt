b5722b982ab85dbcc11e6438d65b30c3a71ce1c9
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-03-07 13:05:53 +0530

    Refactored constants and patterns

---
+++ app/src/main/java/com/example/userinfo/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.userinfo
+
+const val UserName="userName"
+const val Email="email"
+const val PhoneNumber="phoneNumber"
+const val Address="address"
+const val PinCode="pinCode"
+const val Visible="visible"
